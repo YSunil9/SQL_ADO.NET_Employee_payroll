9374b9f90600d38ee2a83cfcf608f8a6348aef50
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-17 23:08:08 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employer Payroll Implementations");
+        }
+    }
+}
